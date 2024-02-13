f29baacf4fe00997bc941bee86f0e9f96cbed3fc
Elias Tazartes <elias.tazartes@gmail.com>
2024-02-13 13:24:58 +0100

    feat: add a landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,47 @@
+import styles from './index.module.css';
+import Link from '@docusaurus/Link';
+import KakarotLogo from './kakarot.svg';
+import DiscordLogo from './discord.svg';
+
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <div className={styles.principal}>
+        <h1 className={styles.title}>
+          Navigate the Kakarot <span className={styles.accent}>Ecosystem</span>
+        </h1>
+        <p className={styles.paragraph}>
+          Discover Kakarot, where impeccable developer experience meets a
+          user-first approach: 100% Ethereum compatiblity plus UX-driven new
+          features (account abstraction, multicall, new EIPs). Dive into our
+          documentation and start building the future of Ethereum, today.
+        </p>
+        <Link className={styles.button} to='/quick-start'>
+          Access the Docs
+        </Link>
+      </div>
+      <footer className={styles.footer}>
+        <a
+          href='https://kakarot.org'
+          className={styles.footeritem}
+          target='_blank'
+        >
+          <KakarotLogo className={styles.icon} />
+          <span className={styles.kakarotlink}>kakarot.org</span>
+        </a>
+        <a
+          href='https://discord.gg/kakarotzkevm'
+          className={styles.footeritem}
+          target='_blank'
+        >
+          <DiscordLogo className={`${styles.discord} ${styles.icon}`} />
+          <div className={styles.footerdiv}>
+            {' '}
+            <span>Question ? </span>
+            <span className={styles.small}>Join our discord</span>
+          </div>
+        </a>
+      </footer>
     </div>
   );
 }
