6fa32aa4771a2991c9e9bf65ccf6f4be99e43e05
Elias Tazartes <66871571+Eikix@users.noreply.github.com>
2024-02-14 14:02:18 +0100

    feat: add lottie and responsiveness (#12)
    * feat: add lottie and responsiveness
    
    * fix body html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,46 +2,56 @@ import styles from './index.module.css';
 import Link from '@docusaurus/Link';
 import KakarotLogo from './kakarot.svg';
 import DiscordLogo from './discord.svg';
+import Lottie from 'lottie-react';
+import animationData from './lottie.json';
 
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
-      <div className={styles.principal}>
-        <h1 className={styles.title}>
-          Navigate the Kakarot <span className={styles.accent}>Ecosystem</span>
-        </h1>
-        <p className={styles.paragraph}>
-          Discover Kakarot zkEVM, a zkRollup where great developer experience
-          meets a user-first approach: 100% Ethereum compatiblity plus UX-driven
-          new features (account abstraction, multicall, new EIPs). Start
-          building the future of Ethereum, today.
-        </p>
-        <Link className={styles.button} to='/quick-start'>
-          Access the Docs
-        </Link>
+      <Lottie
+        className={styles.lottie}
+        animationData={animationData}
+        loop={true}
+      />
+      <div className={styles.content}>
+        <div className={styles.principal}>
+          <h1 className={styles.title}>
+            Navigate the Kakarot{' '}
+            <span className={styles.accent}>Ecosystem</span>
+          </h1>
+          <p className={styles.paragraph}>
+            Discover Kakarot zkEVM, a zkRollup where great developer experience
+            meets a user-first approach: 100% Ethereum compatiblity plus
+            UX-driven new features (account abstraction, multicall, new EIPs).
+            Start building the future of Ethereum, today.
+          </p>
+          <Link className={styles.button} to='/quick-start'>
+            Access the Docs
+          </Link>
+        </div>
+        <footer className={styles.footer}>
+          <a
+            href='https://kakarot.org'
+            className={styles.footeritem}
+            target='_blank'
+          >
+            <KakarotLogo className={styles.icon} />
+            <span className={styles.kakarotlink}>kakarot.org</span>
+          </a>
+          <a
+            href='https://discord.gg/kakarotzkevm'
+            className={styles.footeritem}
+            target='_blank'
+          >
+            <DiscordLogo className={`${styles.discord} ${styles.icon}`} />
+            <div className={styles.footerdiv}>
+              {' '}
+              <span className={styles.medium}>Question ? </span>
+              <span className={styles.small}>Join our discord</span>
+            </div>
+          </a>
+        </footer>
       </div>
-      <footer className={styles.footer}>
-        <a
-          href='https://kakarot.org'
-          className={styles.footeritem}
-          target='_blank'
-        >
-          <KakarotLogo className={styles.icon} />
-          <span className={styles.kakarotlink}>kakarot.org</span>
-        </a>
-        <a
-          href='https://discord.gg/kakarotzkevm'
-          className={styles.footeritem}
-          target='_blank'
-        >
-          <DiscordLogo className={`${styles.discord} ${styles.icon}`} />
-          <div className={styles.footerdiv}>
-            {' '}
-            <span>Question ? </span>
-            <span className={styles.small}>Join our discord</span>
-          </div>
-        </a>
-      </footer>
     </div>
   );
 }
